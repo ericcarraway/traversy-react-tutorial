45ae3f558ef8403953ecd1bc671270875315396a
Eric Carraway <github@ericcarraway.com>
2018-06-04 17:09:20 -0500

    tutorial 18 min mark - can pass data to "Projects"

--- src/App.js
+++ src/App.js
@@ -3,11 +3,32 @@ import Projects from './Components/Projects';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+ 
+    this.state = {
+      projects: [
+        {
+          title: 'Business Website',
+          category: 'Web Design'
+        },
+        {
+          title: 'Social App',
+          category: 'Mobile Development'
+        },
+        {
+          title: 'E-Commerce Shopping Cart',
+          category: 'Web Development'
+        } 
+      ]
+    };
+  }
+
   render() {
     return (
       <div className="App">
         My App
-        <Projects />
+        <Projects projects={this.state.projects} />
       </div>
     );
   }
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 class Projects extends Component {
-  render() {
+  render() {  
     return (
       <div className="Projects">
         My Projects
