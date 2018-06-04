db82017d8dedd35fd776caf616c43359315b3877
Eric Carraway <github@ericcarraway.com>
2018-06-04 17:37:31 -0500

    tutorial 34 min mark - flesh out handleSubmit

--- src/Components/AddProject.js
+++ src/Components/AddProject.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
 
 class AddProject extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+        newProject: {}
+    };
+  }
+
   static defaultProps = {
     categories: ['Web Design', 'Web Development', 'Mobile Development']
   }
 
   handleSubmit(e) {
-    console.log('Submitted');
+    if (this.refs.title.value === '') {
+        alert('Title is required');
+    } else {
+        this.setState({
+            newProject: {
+                title: this.refs.title.value,
+                category: this.refs.category.value
+            }
+        }, function () {
+            console.log(this.state);
+        });
+    }
+
+    console.log(this.refs.title.value);
 
     e.preventDefault();
   }
@@ -19,7 +40,7 @@ class AddProject extends Component {
     return (
       <div>
         <h3>Add Project</h3>
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={this.handleSubmit.bind(this)}>
            <div>
             <label>Title</label><br />
             <input type="text" ref="title" />
